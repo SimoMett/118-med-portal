5ee00e8a2b42bc4437ab4060a7e2f3d59ff4232b
SimoMett <matt.simonetti@hotmail.it>
2024-03-06 17:23:17 +0100

    Added MissionControllerTest in ControlCenter

---
+++ src/Test/ControlCenter/MissionControllerTest.java
@@ -0,0 +1,18 @@
+package src.Test.ControlCenter;
+
+import org.junit.Test;
+
+public class MissionControllerTest
+{
+    @Test
+    public void assignMission()
+    {
+
+    }
+
+    @Test
+    public void updateDispatch()
+    {
+
+    }
+}
