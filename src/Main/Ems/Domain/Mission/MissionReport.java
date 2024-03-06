7e4a78fe75113a982a0dd2dfa697f574ad75c871
SimoMett <matt.simonetti@hotmail.it>
2024-03-06 09:24:17 +0100

    Added methods and timestamp implementations in MissionReport

--- src/Main/Ems/Domain/Mission/MissionReport.java
+++ src/Main/Ems/Domain/Mission/MissionReport.java
@@ -1,7 +1,7 @@
 package src.Main.Ems.Domain.Mission;
 
 import java.sql.Timestamp;
-import java.util.Date;
+import java.time.Instant;
 import java.util.HashMap;
 
 public class MissionReport
@@ -16,19 +16,20 @@ public class MissionReport
     //attributes
     public final ReportType reportType;
     private final String missionId;
-    private Date dateOfCreation;
+    private final Timestamp dateOfCreation;
     private String dispatch;
     private Timestamp lastEdit;
     private boolean isActive;
-    private HashMap<String, Object> dataPairs;
+    private final HashMap<String, Object> dataPairs;
 
     //methods
     public MissionReport(String missionId, ReportType type)
     {
         this.missionId = missionId;
         this.reportType = type;
-        //TODO init dateOfCreation
+        this.dateOfCreation = Timestamp.from(Instant.now());
         this.dataPairs = new HashMap<>();
+        this.isActive = true;
     }
 
     public String getDispatch()
@@ -36,8 +37,21 @@ public class MissionReport
         return dispatch;
     }
 
+    public void updateDispatch(String newDispatch)
+    {
+        this.dispatch = newDispatch;
+    }
+
     public void updateData(String key, Object value)
     {
         dataPairs.replace(key, value);
+        lastEdit = Timestamp.from(Instant.now());
+    }
+
+    public Object getData(String key) throws IllegalArgumentException
+    {
+        if(!dataPairs.containsKey(key))
+            throw new IllegalArgumentException("Provided invalid key for "+reportType.name()+" report: "+key);
+        return dataPairs.get(key);
     }
 }
