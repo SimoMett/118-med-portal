c4da0507b9fa414d4fd1a55f83d588dafe6e308e
SimoMett <matt.simonetti@hotmail.it>
2024-03-14 11:50:58 +0100

    Added MissionStatus enum

---
+++ src/Main/ControlCenter/MissionStatus.java
@@ -0,0 +1,15 @@
+package src.Main.ControlCenter;
+
+public enum MissionStatus
+{
+    MISSION_START,
+    TARGET_REACHED,
+    TREATED_ON_SITE,
+    DEPARTING_CODE1,
+    DEPARTING_CODE2,
+    DEPARTING_CODE3,
+    DEPARTING_CODE4,
+    DESTINATION_REACHED,
+    MISSION_END,
+    RETURNED_TO_HQ
+}
